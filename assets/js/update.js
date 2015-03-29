f72682a7ead94942ee69bf47611e88e96248b919
officer <officer@enterprise.devices.christran.in>
2015-03-29 12:48:53 -0400

    assets

--- assets/js/update.js
+++ assets/js/update.js
@@ -7,7 +7,7 @@ $.ajax({
 		$.each(r.log, function(i, log) {
 			$("#events").prepend("<tr><td>"+log.entry.id+"</td><td>"+log.entry.measurement+"</td><td>"+log.entry.userid+"</td><td>"+log.entry.tr+"</td></tr>");
 		});
-	}
+	},
 	error: function(xhs, textStatus, errorThrown) {
 		if(xhr.status === 404) {
 			$("#errorMessage").show();
