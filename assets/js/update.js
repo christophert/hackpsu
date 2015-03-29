efef4679115b91740222a2103ef30e9d09276cac
officer <officer@enterprise.devices.christran.in>
2015-03-29 12:46:52 -0400

    assets

---
+++ assets/js/update.js
@@ -0,0 +1,17 @@
+$.ajax({
+	type: "POST",
+	url: "/api/log.php",
+	timeout: 2000,
+	cache: false,
+	success: function(r) {
+		$.each(r.log, function(i, log) {
+			$("#events").prepend("<tr><td>"+log.entry.id+"</td><td>"+log.entry.measurement+"</td><td>"+log.entry.userid+"</td><td>"+log.entry.tr+"</td></tr>");
+		});
+	}
+	error: function(xhs, textStatus, errorThrown) {
+		if(xhr.status === 404) {
+			$("#errorMessage").show();
+			$("#errorMessage").html("Error: "+errorThrown);
+		}
+	}
+});
