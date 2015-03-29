ed4474f2479d7fafb58061f77cc54ddba02ea977
hackpsu-surface <hackpsu-surface@devices.christran.in>
2015-03-29 11:32:56 -0400

    api and index

---
+++ api/log.php
@@ -0,0 +1,16 @@
+<?php
+require("../../db.php");
+function getAllEntries() {
+	$stmt = $conn->prepare("select * from events");
+	if($stmt->execute()) {
+		$response = $stmt->fetch(PDO::FETCH_ASSOC);
+		return $reponse;
+	}
+}
+
+session_start();
+$response = getAllEntries();
+header("Content-Type: application/json");
+$end['log'] = $reponse;
+echo json_encode($end);
+?>
